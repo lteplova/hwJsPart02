854fa8496ff592935d685002efb886d4eb5bb6bc
lteplova <lteplova@gmail.com>
2018-12-25 14:26:40 +0300

    fixes

---
+++ browser/slider/index.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+const images = [
+  "https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-jump.png",
+  "https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-on-foot.png",
+  "https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-playground.png",
+  "https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax-top-view.png",
+  "https://netology-code.github.io/hj-homeworks/browser/slider/i/airmax.png",
+];
+const slider = document.getElementById("slider");
+let current = 0;
+
+function show() {
+  slider.src = images[current];
+  current == images.length - 1 ? (current = 0) : current++;
+}
+
+show();
+setInterval(show, 1000);
---
+++ event-object/piano/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const lower = [
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/lower/first.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/lower/second.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/lower/third.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/lower/fourth.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/lower/fifth.mp3'
+];
+
+const middle = [
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/middle/first.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/middle/second.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/middle/third.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/middle/fourth.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/middle/fifth.mp3'
+];
+
+const higher = [
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/higher/first.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/higher/second.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/higher/third.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/higher/fourth.mp3',
+    'https://netology-code.github.io/hj-homeworks/event-object/piano/sounds/higher/fifth.mp3'
+];
+
+
+
+function lower() {
+
+}
+document.addEventListener('keydown', lower);
