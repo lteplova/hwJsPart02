f4ed384ba82ace514dbb3c9481f913e396bb362d
Liudmila Teplova <lteplova@gmail.com>
2018-12-27 23:52:50 +0300

    currentTime = 0

--- html-element-collection/drum-machine/index.js
+++ html-element-collection/drum-machine/index.js
@@ -5,6 +5,7 @@ const buttons = document.getElementsByClassName("drum-kit__drum");
 for (const button of buttons ) {
     button.onclick = function() {
       const audio = this.getElementsByTagName('audio')[0];
+      audio.currentTime = 0
       audio.play();
     }
 }
