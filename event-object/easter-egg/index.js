bbf1cb343d2be50270530f1916c1a3e4c8841f7d
Liudmila Teplova <lteplova@gmail.com>
2019-01-07 21:10:32 +0300

    fix_note

--- event-object/easter-egg/index.js
+++ event-object/easter-egg/index.js
@@ -1,24 +1,32 @@
 "use strict";
 
-const navTag = document.getElementsByTagName('nav')[0];
+const navTag = document.getElementsByTagName("nav")[0];
 
-function openCloseMenu(event){
-    if(!event.ctrlKey && !event.altKey) {
-        return;
-    }
-    if(event.code =='KeyT') {
-    navTag.classList.toggle('visible');
-    }
+function openCloseMenu(event) {
+  if (event.ctrlKey && event.altKey && event.code == "KeyT") {
+    navTag.classList.toggle("visible");
+  }
 }
 
-document.addEventListener('keypress', openCloseMenu);
+document.addEventListener("keypress", openCloseMenu);
 
-const classSecret = document.getElementsByClassName('secret')[0];
+const classSecret = document.getElementsByClassName("secret")[0];
 
 function secret(event) {
-    if (event.code == ('KeyY' && 'KeyT' && 'KeyN' && 'KeyJ' && 'KeyK' && 'KeyJ' && 'KeyU' && 'KeyB' && 'KeyZ')) {
-        classSecret.classList.add('visible');
-    }
+  if (
+    event.code ==
+    ("KeyY" &&
+      "KeyT" &&
+      "KeyN" &&
+      "KeyJ" &&
+      "KeyK" &&
+      "KeyJ" &&
+      "KeyU" &&
+      "KeyB" &&
+      "KeyZ")
+  ) {
+    classSecret.classList.add("visible");
+  }
 }
 
-document.addEventListener('keydown', secret);
+document.addEventListener("keydown", secret);
