ab9d92f526b09a953a92f96acc36fedba17b6694
Liudmila Teplova <lteplova@gmail.com>
2019-01-12 22:34:04 +0300

    fixed_2

--- dom/shop/index.js
+++ dom/shop/index.js
@@ -3,13 +3,16 @@
 const cartButton = document.querySelectorAll(".add");
 const totalSumEl = document.querySelector("#cart-total-price");
 const totalCountEl = document.querySelector("#cart-count");
+let totalSum = 0;
 
 Array.from(cartButton).forEach(element => {
   element.addEventListener("click", addCart);
 });
 
 function addCart(event) {
-  totalSumEl.innerHTML =
-    parseInt(totalSumEl.innerHTML) + parseInt(event.target.dataset.price);
+  totalSum = 
+    parseInt(totalSum) + parseInt(event.target.dataset.price);
+    totalSumEl.innerHTML = getPriceFormatted(totalSum); 
+
   totalCountEl.innerHTML = parseInt(totalCountEl.innerHTML) + 1;
 }
