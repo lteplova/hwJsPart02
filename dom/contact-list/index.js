38d77f93b62b3c10c1e99996ac2daaf5eabf400d
Liudmila Teplova <lteplova@gmail.com>
2019-01-12 22:28:15 +0300

    fixed_1

--- dom/contact-list/index.js
+++ dom/contact-list/index.js
@@ -1,16 +1,24 @@
 "use strict";
 
-const data = JSON.parse(loadContacts());
+let data = [];
+
+try {
+  data = JSON.parse(loadContacts());
+} catch (e) {
+  console.error(e);
+}
+
 const ulEl = document.querySelector("#container > div.list-view > ul");
-let list = '';
 
 function render(user) {
-    return ` <li data-email="${user.email}" data-phone="${user.phone}">
+  return ` <li data-email="${user.email}" data-phone="${user.phone}">
     <strong>${user.name}</strong>
-  </li>`
+  </li>`;
 }
-data.forEach(element => {
-list += render(element);
-});
 
-ulEl.innerHTML = list;
+ulEl.innerHTML = data.reduce((previousValue, currentValue) => {
+  if (typeof previousValue === 'object') {
+    previousValue = render(previousValue);
+  }
+  return previousValue + render(currentValue);
+});
