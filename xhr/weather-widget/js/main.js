c4ba1777b49228f0a81c1c011479899d637cf508
Liudmila Teplova <lteplova@gmail.com>
2019-01-13 23:10:34 +0300

    task_1

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,11 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://neto-api.herokuapp.com/weather', false);
+request.open("GET", "https://neto-api.herokuapp.com/weather", true);
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
+
+request.addEventListener("load", onLoad);
+function onLoad() {
+  if (request.status === 200) {
+    const response = JSON.parse(request.responseText);
+    setData(response);
+  }
 }
