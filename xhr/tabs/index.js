7adbed1419c710f1fa3de6592dbaa272077144db
Liudmila Teplova <lteplova@gmail.com>
2019-01-14 22:38:04 +0300

    task2fix

--- xhr/tabs/index.js
+++ xhr/tabs/index.js
@@ -1,34 +1,51 @@
 "use strict";
 
-const aEls = document.querySelectorAll('nav > a');
-const request = new XMLHttpRequest();
+const aEls = document.querySelectorAll("nav > a");
 const activeTabContent = document.querySelector("#content");
+const preloaderEl = document.querySelector("#preloader");
+
+document.addEventListener("DOMContentLoaded", showTabEmail);
+
+function showTabEmail(event) {
+  const aEl = document.querySelector("nav > a.active");
+  loadData(aEl.href);
+}
 
 Array.from(aEls).forEach(element => {
-    element.addEventListener('click', tabClick)  
+  element.addEventListener("click", tabClick);
 });
 
 function tabClick(event) {
-event.preventDefault();
-resetActive(aEls);
-event.target.classList.toggle("active");
-loadData(event.target.href);
+  event.preventDefault();
+  resetActive(aEls);
+  event.target.classList.toggle("active");
+  activeTabContent.innerHTML = '';
+  loadData(event.target.href);
 }
 
 function resetActive(elements) {
-    Array.from(elements).forEach(element => {
-        element.classList.remove("active");
-    }); 
+  Array.from(elements).forEach(element => {
+    element.classList.remove("active");
+  });
 }
 
 function loadData(url) {
-request.open("GET", "url", true);
-console.log(url);
-request.send();
-request.addEventListener("load", onLoad);
-}
+  const request = new XMLHttpRequest();
+  request.addEventListener("loadstart", onLoadStart);
+  request.addEventListener("loadend", onLoadEnd);
+  request.addEventListener("load", onLoad);
+  request.open("GET", url, true);
+  request.send();
 
-
-function onLoad() {
-activeTabContent.innerHTML = loadData(url).href;
+  function onLoad() {
+    activeTabContent.innerHTML = request.responseText;
   }
+  
+  function onLoadEnd() {
+    preloaderEl.classList.add("hidden");
+  }
+
+  function onLoadStart() {
+      preloaderEl.classList.remove("hidden");
+  }
+}
