c6e7edd3195322f345fe8a639122b8490d453fbe
Людмила Теплова <l.teplova@teplova.drweb.lan>
2019-01-16 14:46:30 +0300

    task3_books_edit

--- xhr/books-catalog/index.js
+++ xhr/books-catalog/index.js
@@ -4,16 +4,23 @@ const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://neto-api.herokuapp.com/book/", true);
 xhr.send();
 xhr.addEventListener("load", onLoad);
+xhr.addEventListener("error", onError);
 const contentEl = document.querySelector("#content");
 let data = [];
+
+function onError() {
+  console.log(`Ошибка загрузки данных: ${xhr.status}, ${xhr.statusText}`);
+}
+
 
 function onLoad() {
   if (xhr.status === 200) {
     data = JSON.parse(xhr.responseText);
     contentEl.innerHTML = data.reduce(
-      (str, currentValue) => str + render(currentValue),
-      ""
-    );
+      (str, currentValue) => str + render(currentValue), "");
+  }
+  else {
+    onError();
   }
 }
 
