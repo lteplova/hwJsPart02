96153e33150392039b052eaffa12d42aa1f52f0a
Liudmila Teplova <lteplova@gmail.com>
2019-01-17 19:07:20 +0300

    task-list

--- html-forms/todo-list/index.js
+++ html-forms/todo-list/index.js
@@ -1,14 +1,27 @@
 "use strict";
 
-const inputEl = document.querySelectorAll('input');
+const inputEl = document.querySelectorAll(".list-block input");
+const otputEl = document.querySelector("output");
+const listEl = document.querySelector(".list-block");
+
 let totalSum = 0;
 
+counter();
 Array.from(inputEl).forEach(element => {
+  element.addEventListener("change", counter);
+});
+
+function counter(event) {
+  totalSum = 0;
+  Array.from(inputEl).forEach(element => {
     if (element.checked == true) {
-        counter();
+      totalSum++;
     }
-    console.log(element.checked);
-    //element.addEventListener("click", counter);
   });
-  
-function counter(event) {}
+  otputEl.innerText = `${totalSum} из ${inputEl.length}`;
+  if (totalSum == inputEl.length) {
+    listEl.classList.toggle("complete");
+  } else {
+    listEl.classList.remove("complete");
+  }
+}
