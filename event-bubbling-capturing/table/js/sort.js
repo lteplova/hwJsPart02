a251cbce880173f0725265db8bcdac5ec9cd63e7
Liudmila Teplova <lteplova@gmail.com>
2019-01-23 22:37:53 +0300

    menu+sort

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,9 @@
-'use strict';
+"use strict";
 
 function handleTableClick(event) {
-  
+  const tableEl = document.querySelector("table");
+  const header = event.target;
+  header.dataset.dir ? (header.dataset.dir *= -1) : (header.dataset.dir = 1);
+  tableEl.dataset.sortBy = header.dataset.propName;
+  sortTable(tableEl.dataset.sortBy, header.dataset.dir);
 }
