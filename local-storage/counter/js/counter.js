f2f815073f0c92d1a6cba83852fa3fe69367def4
Liudmila Teplova <lteplova@gmail.com>
2019-01-24 23:04:07 +0300

    counter_done

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,29 @@
-'use strict';
+"use strict";
+
+const counterEl = document.querySelector("#counter");
+const buttonIncrEl = document.querySelector("#increment");
+const buttonDecrEl = document.querySelector("#decrement");
+const buttonResetEl = document.querySelector("#reset");
+let counter = localStorage.getItem("counter") || 0;
+
+counterEl.innerHTML = counter;
+
+buttonIncrEl.addEventListener("click", event => {
+  counter++;
+  setState(counter);
+});
+
+buttonDecrEl.addEventListener("click", event => {
+  counter > 0 ? counter-- : (counter = 0);
+  setState(counter);
+});
+
+buttonResetEl.addEventListener("click", event => {
+  counter = 0;
+  setState(counter);
+});
+
+function setState(counter) {
+  localStorage.setItem("counter", counter);
+  counterEl.innerHTML = counter;
+}
