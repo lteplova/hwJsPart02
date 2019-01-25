2e57837a105124a21c37ac98b3a706568749f5ba
Liudmila Teplova <lteplova@gmail.com>
2019-01-25 22:11:21 +0300

    Sort fix

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,9 +1,12 @@
 "use strict";
 
 function handleTableClick(event) {
+  if (!event.target.classList.contains('prop__name')) return;
+  
   const tableEl = document.querySelector("table");
   const header = event.target;
   header.dataset.dir ? (header.dataset.dir *= -1) : (header.dataset.dir = 1);
   tableEl.dataset.sortBy = header.dataset.propName;
   sortTable(tableEl.dataset.sortBy, header.dataset.dir);
+  
 }
