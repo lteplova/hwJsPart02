f346d75b88b9b5c9cb93901ef03ef65f10817f48
Liudmila Teplova <lteplova@gmail.com>
2019-01-26 20:58:18 +0300

    shop-fixed

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -3,21 +3,15 @@
 const itemListEl = document.querySelector(".items-list");
 const showMoreEl = document.querySelector(".show-more");
 
-showMoreEl.addEventListener("click", onClickShowMore);
+itemListEl.addEventListener("click", onClick);
 
-function onclick(event) {
+function onClick(event) {
+  if (!event.target.classList.contains("add-to-cart")) return;
   event.preventDefault();
+
   addToCart(
     event.target,
     event.target.dataset.title,
     event.target.dataset.price
   );
 }
-
-function onClickShowMore(event) {
-  const buttonEls = document.querySelectorAll(".add-to-cart");
-  Array.from(buttonEls).forEach(element => {
-    element.addEventListener("click", onclick);
-  });
-}
-onClickShowMore();
