29c0590a71d0d5829501a864f659cd49d61755ec
Людмила Теплова <l.teplova@teplova.drweb.lan>
2019-01-28 16:32:44 +0300

    card-edits

--- local-storage/cart/js/cart.js
+++ local-storage/cart/js/cart.js
@@ -1,1 +1,53 @@
 'use strict';
+
+const xhrColour = new XMLHttpRequest();
+let url;
+let data = [];
+
+showDataColour();
+showDataSize();
+
+function loadData(url) {
+    xhrColour.addEventListener("load", onLoad);
+    xhrColour.open('GET', url, true);
+    xhrColour.send();
+
+    function onLoad() {
+
+        if (xhrColour.status === 200) {
+            try {
+                data = JSON.parse(xhrColour.responseText);
+                return data;
+            } catch (e) {
+                console.error(`JSON invalid ${e}`);
+            }
+        } else {
+            console.log(`Ошибка загрузки данных: ${xhrColour.status}, ${xhrColour.statusText}`);
+        }
+    }
+  
+}
+
+loadData('https://neto-api.herokuapp.com/cart/colors');
+
+
+function showDataColour(data) {
+    loadData(url = 'https://neto-api.herokuapp.com/cart/colors');
+
+    data.forEach(element => {
+        console.log(element);
+    });
+}
+
+
+function showDataSize(data) {
+    loadData(url = 'https://neto-api.herokuapp.com/cart/sizes');
+    data.forEach(element => {
+        console.log(element);
+    });
+}
+
+
+
+
+// https://neto-api.herokuapp.com/cart
