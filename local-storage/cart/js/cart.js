5d84cbc2c2834698b2128d42b1eeb0564353bb64
Liudmila Teplova <lteplova@gmail.com>
2019-01-28 23:22:44 +0300

    Color & Size

--- local-storage/cart/js/cart.js
+++ local-storage/cart/js/cart.js
@@ -1,53 +1,112 @@
-'use strict';
-
-const xhrColour = new XMLHttpRequest();
-let url;
-let data = [];
+"use strict";
+const colorEl = document.querySelector("#colorSwatch");
+const sizeEl = document.querySelector("#sizeSwatch");
+const cartEl = document.querySelector("#quick-cart");
 
 showDataColour();
 showDataSize();
+showDataCart();
 
-function loadData(url) {
-    xhrColour.addEventListener("load", onLoad);
-    xhrColour.open('GET', url, true);
-    xhrColour.send();
+function addCart(data) {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", onLoad);
+    xhr.open("POST", 'https://neto-api.herokuapp.com/cart', true);
+    xhr.send(data);
+}
 
-    function onLoad() {
+function loadData(url, cb) {
+  let data = [];
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", onLoad);
+  xhr.open("GET", url, true);
+  xhr.send();
 
-        if (xhrColour.status === 200) {
-            try {
-                data = JSON.parse(xhrColour.responseText);
-                return data;
-            } catch (e) {
-                console.error(`JSON invalid ${e}`);
-            }
-        } else {
-            console.log(`Ошибка загрузки данных: ${xhrColour.status}, ${xhrColour.statusText}`);
-        }
+  function onLoad() {
+    if (xhr.status === 200) {
+      try {
+        data = JSON.parse(xhr.responseText);
+        //return data;
+        cb(data);
+      } catch (e) {
+        console.error(`JSON invalid ${e}`);
+      }
+    } else {
+      console.log(`Ошибка загрузки данных: ${xhr.status}, ${xhr.statusText}`);
     }
-  
+  }
 }
 
-loadData('https://neto-api.herokuapp.com/cart/colors');
-
-
-function showDataColour(data) {
-    loadData(url = 'https://neto-api.herokuapp.com/cart/colors');
-
-    data.forEach(element => {
-        console.log(element);
+function showDataColour() {
+  loadData("https://neto-api.herokuapp.com/cart/colors", data => {
+    data.forEach((item, index) => {
+      console.log(item);
+      renderColor(colorEl, item, index);
     });
+  });
 }
 
-
-function showDataSize(data) {
-    loadData(url = 'https://neto-api.herokuapp.com/cart/sizes');
-    data.forEach(element => {
-        console.log(element);
+function showDataSize() {
+  loadData("https://neto-api.herokuapp.com/cart/sizes", data => {
+    data.forEach((item, index) => {
+      console.log(item);
+      renderSize(sizeEl, item, index);
     });
+  });
 }
 
+function showDataCart() {
+    loadData("https://neto-api.herokuapp.com/cart", data => {
+      data.forEach((item, index) => {
+        console.log(item);
+        renderCart(cartEl, item, index);
+      });
+    });
+  }
 
+function renderColor(colorContainerEl, data, index) {
+  const divEl = document.createElement("div");
+  divEl.dataset.value = data.type;
+  divEl.classList.add(
+    "swatch-element",
+    "color",
+    data.type,
+    data.isAvailable ? "available" : "soldout"
+  );
+  divEl.innerHTML = `<div class="tooltip">${data.title}</div>
+    <input quickbeam="color" id="swatch-${index}-${data.type}" type="radio" name="color" value="${data.type}" checked ${data.isAvailable ? '' : 'disabled'}>
+    <label for="swatch-${index}-${data.type}" style="border-color: ${data.type};">
+      <span style="background-color: ${data.type};"></span>
+      <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
+    </label>`;
 
+  colorContainerEl.appendChild(divEl);
+}
 
-// https://neto-api.herokuapp.com/cart
+function renderSize(sizeContainerEl, data, index) {
+  const divEl = document.createElement("div");
+  divEl.dataset.value = data.type;
+  divEl.classList.add(
+    "swatch-element",
+    "plain",
+    data.type,
+    data.isAvailable ? "available" : "soldout"
+  );
+  divEl.innerHTML = `<input id="swatch-${index}-${data.type}" type="radio" name="size" value="${data.type}" ${data.isAvailable ? '' : 'disabled'}>
+    <label for="swatch-${index}-${data.type}">${data.type}
+      <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
+    </label>`;
+
+  sizeContainerEl.appendChild(divEl);
+}
+
+function renderCart(cartContainerEl, data, index) {
+    cartContainerEl.innerHTML = `<div class="quick-cart-product quick-cart-product-static" id="quick-cart-product-${data.id}" style="opacity: 1;">
+    <div class="quick-cart-product-wrap">
+      <img src="${data.pic}" title="${data.title}">
+      <span class="s1" style="background-color: #000; opacity: .5">$${data.price}.00</span>
+      <span class="s2"></span>
+    </div>
+    <span class="count hide fadeUp" id="quick-cart-product-count-${data.id}">${data.quantity}</span>
+    <span class="quick-cart-product-remove remove" data-id="${data.id}"></span>
+  </div>`;
+}
--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,55 @@
-'use strict';
+"use strict";
+
+const enterForm = document.querySelector(".sign-in-htm");
+const regForm = document.querySelector(".sign-up-htm");
+const loginForm = document.querySelector(".login-form");
+const activeTabEls = document.querySelectorAll(".login-html > input");
+
+Array.from(activeTabEls).forEach(item => {
+  const form = document.querySelector("." + item.className + "-htm");
+  form.addEventListener("submit", onSubmit);
+});
+
+function onSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+
+  let data = {};
+  formData.forEach((val, key) => {
+    data[key] = val;
+  });
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", e => {
+    const errorMessage = event.target.querySelector(".error-message");
+    const response = JSON.parse(xhr.response);
+    if (response.error) {
+      errorMessage.innerHTML = response.message;
+    } else {
+      switch (event.target.className) {
+        case "sign-in-htm":
+          errorMessage.innerHTML = `Пользователь ${
+            response.name
+          } успешно авторизован`;
+          break;
+        case "sign-up-htm":
+          errorMessage.innerHTML = `Пользователь ${
+            response.name
+          } успешно зарегистрирован`;
+          break;
+      }
+    }
+  });
+
+  switch (event.target.className) {
+    case "sign-in-htm":
+      xhr.open("POST", "https://neto-api.herokuapp.com/signin");
+      break;
+    case "sign-up-htm":
+      xhr.open("POST", "https://neto-api.herokuapp.com/signup");
+      break;
+  }
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify(data));
+}
