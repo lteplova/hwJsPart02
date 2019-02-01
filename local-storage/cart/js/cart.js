095921802188a139be9365aa5f10ea5c38a02f57
Людмила Теплова <l.teplova@teplova.drweb.lan>
2019-02-01 10:40:25 +0300

    comments

--- local-storage/cart/js/cart.js
+++ local-storage/cart/js/cart.js
@@ -11,8 +11,7 @@ Array.from(thumbImagEls).forEach(item => {
 
 function preventOpen(event) {
   event.preventDefault();
-  event.target.parentElement.classList.toogle("active");
-}
+  }
 
 const buttonAddToCart = document.querySelector("#AddToCart");
 buttonAddToCart.addEventListener("click", onClickAddToCart);
@@ -22,6 +21,7 @@ showDataColour();
 showDataSize();
 showDataCart();
 
+// функция загрузки данных
 function loadData(url, cb) {
   let data = [];
   const xhr = new XMLHttpRequest();
@@ -33,7 +33,6 @@ function loadData(url, cb) {
     if (xhr.status === 200) {
       try {
         data = JSON.parse(xhr.responseText);
-        //return data;
         cb(data);
       } catch (e) {
         console.error(`JSON invalid ${e}`);
@@ -44,6 +43,7 @@ function loadData(url, cb) {
   }
 }
 
+// получениt списка доступных цветов
 function showDataColour() {
   loadData("https://neto-api.herokuapp.com/cart/colors", data => {
     data.forEach((item, index) => {
@@ -53,6 +53,7 @@ function showDataColour() {
   });
 }
 
+// получениt списка доступных размеров
 function showDataSize() {
   loadData("https://neto-api.herokuapp.com/cart/sizes", data => {
     data.forEach((item, index) => {
@@ -62,6 +63,7 @@ function showDataSize() {
   });
 }
 
+//получение содержимого корзины
 function showDataCart() {
   loadData("https://neto-api.herokuapp.com/cart", data => {
     data.forEach((item, index) => {
@@ -86,6 +88,7 @@ function showDataCart() {
  
 }
 
+//cниппет варианта цвета 
 function renderColor(colorContainerEl, data, index) {
   const checked =
     localStorage.getItem("color") == `swatch-${index}-${data.type}`
@@ -100,14 +103,8 @@ function renderColor(colorContainerEl, data, index) {
     data.isAvailable ? "available" : "soldout"
   );
   divEl.innerHTML = `<div class="tooltip">${data.title}</div>
-    <input quickbeam="color" id="swatch-${index}-${
-    data.type
-  }" type="radio" name="color" onChange="changeStorage(this, 'color')" value="${
-    data.type
-  }" ${checked} ${data.isAvailable ? "" : "disabled"}>
-    <label for="swatch-${index}-${data.type}" style="border-color: ${
-    data.type
-  };">
+    <input quickbeam="color" id="swatch-${index}-${data.type}" type="radio" name="color" onChange="changeStorage(this, 'color')" value="${data.type}" ${checked} ${data.isAvailable ? "" : "disabled"}>
+    <label for="swatch-${index}-${data.type}" style="border-color: ${data.type};">
       <span style="background-color: ${data.type};"></span>
       <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
     </label>`;
@@ -115,6 +112,7 @@ function renderColor(colorContainerEl, data, index) {
   colorContainerEl.appendChild(divEl);
 }
 
+//cниппет варианта размера 
 function renderSize(sizeContainerEl, data, index) {
   const checked =
     localStorage.getItem("size") == `swatch-${index}-${data.type}`
@@ -128,11 +126,7 @@ function renderSize(sizeContainerEl, data, index) {
     data.type,
     data.isAvailable ? "available" : "soldout"
   );
-  divEl.innerHTML = `<input id="swatch-${index}-${
-    data.type
-  }" type="radio" name="size" onChange="changeStorage(this, 'size')" value="${
-    data.type
-  }" ${checked} ${data.isAvailable ? "" : "disabled"}>
+  divEl.innerHTML = `<input id="swatch-${index}-${data.type}" type="radio" name="size" onChange="changeStorage(this, 'size')" value="${data.type}" ${checked} ${data.isAvailable ? "" : "disabled"}>
     <label for="swatch-${index}-${data.type}">${data.type}
       <img class="crossed-out" src="https://neto-api.herokuapp.com/hj/3.3/cart/soldout.png?10994296540668815886">
     </label>`;
@@ -140,28 +134,25 @@ function renderSize(sizeContainerEl, data, index) {
   sizeContainerEl.appendChild(divEl);
 }
 
+//cниппет товара в корзине 
 function renderCart(cartContainerEl, data, index) {
-  cartContainerEl.innerHTML = `<div class="quick-cart-product quick-cart-product-static" id="quick-cart-product-${
-    data.id
-  }" style="opacity: 1;">
+  cartContainerEl.innerHTML = `<div class="quick-cart-product quick-cart-product-static" id="quick-cart-product-${data.id}" style="opacity: 1;">
     <div class="quick-cart-product-wrap">
       <img src="${data.pic}" title="${data.title}">
-      <span class="s1" style="background-color: #000; opacity: .5">$${
-        data.price
-      }.00</span>
+      <span class="s1" style="background-color: #000; opacity: .5">$${data.price}.00</span>
       <span class="s2"></span>
     </div>
-    <span class="count hide fadeUp" id="quick-cart-product-count-${data.id}">${
-    data.quantity
-  }</span>
+    <span class="count hide fadeUp" id="quick-cart-product-count-${data.id}">${data.quantity}</span>
     <span class="quick-cart-product-remove remove" onClick="removeCartItem(this)" data-id="${data.id}"></span>
   </div>`;
 }
 
+// сохранение данных о выбранном цвете и размере в localStorage
 function changeStorage(el, key) {
   localStorage.setItem(key, el.id);
 }
 
+// удаление товара из корзины
 function removeCartItem(el) {
     console.log(el.dataset.id);
     const xhr = new XMLHttpRequest();
@@ -171,6 +162,7 @@ function removeCartItem(el) {
 
 }
 
+// добавить товар в корзину
 function onClickAddToCart(event) {
   const xhr = new XMLHttpRequest();
   const errorMessage = event.target.querySelector(".error-message");
