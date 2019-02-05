49fe80fc272d5852654dba26ca1fb55b68b2e50f
Liudmila Teplova <lteplova@gmail.com>
2019-02-05 23:23:23 +0300

    Animate

--- canvas/background/js/background.js
+++ canvas/background/js/background.js
@@ -4,55 +4,101 @@ const canvasEl = document.querySelector("#wall");
 canvasEl.width = window.innerWidth;
 canvasEl.height = window.innerHeight;
 const ctx = canvasEl.getContext("2d");
+let shapes = [];
 
 function rand(from, to, fixed = 0) {
   return (Math.random() * (from - to) + to).toFixed(fixed);
 }
 
-function nextPoint(x, y, time) {
-    return {
-      x: x + Math.sin((50 + x + (time / 10)) / 100) * 3,
-      y: y + Math.sin((45 + x + (time / 10)) / 100) * 4
-    };
+class Shape {
+  constructor(context, canvas) {
+    this.canvas = canvas;
+    this.ctx = context;
+    this.x = +rand(0, canvas.width);
+    this.y = +rand(0, canvas.height);
+    this.size = rand(0.1, 0.6, 1);
+    this.ctx.strokeStyle = "#ffffff";
+    this.ctx.lineWidth = 5 * this.size;
+    this.newX = null;
+    this.newY = null;
   }
 
-function circle(size, canvas) {
-  ctx.beginPath();
-  const x = +rand(0, canvas.width);
-  const y = +rand(0, canvas.height);
-  const radius = 12 * size;
-  ctx.lineWidth = 5 * size;
-  ctx.strokeStyle = "#ffffff";
-  ctx.arc(x, y, radius, 0, Math.PI * 2);
-  ctx.stroke();
+  nextPoint(variant) {
+    const time = Date.now();
+    if (variant) {
+      this.newX = this.x + Math.sin((50 + this.x + time / 10) / 100) * 3;
+      this.newY = this.y + Math.sin((45 + this.x + time / 10) / 100) * 4;
+    } else {
+      this.newX = this.x + Math.sin((this.x + time / 10) / 100) * 5;
+      this.newY = this.y + Math.sin((10 + this.x + time / 10) / 100) * 2;
+    }
+  }
+
+  render() {
+    //this.nextPoint(!!rand(0,1));
+  }
 }
 
-function cross(size, canvas) {
-    const x = +rand(0, canvas.width);
-    const y = +rand(0, canvas.height);
-    //const { x, y } = nextPoint(100, 100, Date.now());
-    const length = size * 20;
-    ctx.strokeStyle = "#ffffff";
-    ctx.lineWidth = 5 * size;
+class Circle extends Shape {
+  constructor(context, canvas) {
+    super(context, canvas);
+    this.radius = 12 * this.size;
+  }
 
-    ctx.rotate(rand(0, 360));
+  render() {
+    super.nextPoint(1);
+    super.render();
+    this.ctx.beginPath();
+    this.ctx.arc(this.newX, this.newY, this.radius, 0, 2 * Math.PI);
+    this.ctx.stroke();
+  }
+}
 
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + length, y);
-    ctx.stroke();
+class Cross extends Shape {
+  constructor(context, canvas) {
+    super(context, canvas);
+    this.width = 20 * this.size;
+    this.offset = this.width / 2;
+    this.angel = +rand(0, 360);
+  }
+
+  render() {
+    super.nextPoint(0);
+    super.render();
+    ctx.translate(this.newX, this.newY);
+    ctx.rotate(this.angel * Math.PI / 180);
     
-    ctx.beginPath();
-    ctx.moveTo(x + length/2, y - length/2);
-    ctx.lineTo(x + length/2, y + length/2);
+    this.ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x + this.width, this.y);
     ctx.stroke();
+
+    this.ctx.beginPath();
+    ctx.moveTo(this.x + this.offset, this.y - this.offset);
+    ctx.lineTo(this.x + this.offset, this.y + this.offset);
+    ctx.stroke();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);  
+  }
 }
+
 
 function createShapes(volume, canvas) {
   for (let i = 0; i < volume; i++) {
-    circle(rand(0.1, 0.6, 1), canvas);
-    cross(rand(0.1, 0.6, 1), canvas);
+    let shape = new Circle(ctx, canvas);
+    if (i % 2 == 0) {
+      shape = new Cross(ctx, canvas);
+    }
+    shapes.push(shape);
   }
+  animate();
 }
 
-createShapes(100, canvasEl);
+function animate() {
+  ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+  shapes.forEach(shape => {
+    shape.render();
+  });
+  setTimeout(animate, 20/1000);
+}
+
+createShapes(200, canvasEl);
