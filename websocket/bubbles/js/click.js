5625655485a9db944befc14847c15ba774b99927
Людмила Теплова <l.teplova@teplova.drweb.lan>
2019-02-05 15:06:45 +0300

    bubbles-done

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+const clickEl = document.querySelector('body');
+const coords = {};
+
+clickEl.addEventListener('click', onclick);
+
+function onclick(event) {
+    coords.x = event.offsetX;
+    coords.y = event.offsetY;
+    connection.send(JSON.stringify(coords));
+}
+
+showBubbles(connection);
+
