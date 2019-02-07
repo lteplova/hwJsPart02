0c4bdae653055aa0debfc8a82eb2dcade2a9f6be
Liudmila Teplova <lteplova@gmail.com>
2019-02-07 22:48:51 +0300

    comet draw fix

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -2,9 +2,9 @@
 
 const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
 
-ws.addEventListener('open', editor.addEventListener('update', (event) => {
-    event.canvas.toBlob(blob => ws.send(blob))
-}));
-
-
+ws.addEventListener('open', () => { 
+    editor.addEventListener('update', (edEvent) => {
+        edEvent.canvas.toBlob(blob => ws.send(blob))
+    });
+});
 
