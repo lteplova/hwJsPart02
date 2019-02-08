ae4979228a792c4424bce84bf60ee397fc070c37
Людмила Теплова <l.teplova@teplova.drweb.lan>
2019-02-08 11:24:03 +0300

    comet-draw

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -3,7 +3,7 @@
 const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
 
 ws.addEventListener('open', editor.addEventListener('update', (event) => {
-    event.canvas.toBlob(blob => ws.send(blob))
+    event.canvas.toBlob((blob) => ws.send(blob))
 }));
 
 
