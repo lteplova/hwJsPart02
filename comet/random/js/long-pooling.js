718301e2d35761ffd08d1f8c2813ba5fd1b94e31
Liudmila Teplova <lteplova@gmail.com>
2019-02-08 22:31:32 +0300

    commet-ranodom-done

--- comet/random/js/long-pooling.js
+++ comet/random/js/long-pooling.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+
+const longPoolingtEl = document.querySelector(".long-pooling");
+const divLongPoolEls = longPoolingtEl.children;
+
+const xhrLP = new XMLHttpRequest();
+xhrLP.addEventListener("load", onLoad);
+
+function onLoad() {
+  const num = +xhrLP.responseText;
+  Array.from(divLongPoolEls).forEach(item => {
+    item.classList.remove("flip-it");
+  });
+  divLongPoolEls[num].classList.toggle("flip-it");
+  console.log(+xhrLP.responseText);
+}
+
+function pool() {
+  xhrLP.open("GET", "https://neto-api.herokuapp.com/comet/long-pooling", true);
+  xhrLP.send();
+
+  setTimeout(pool, 5000);
+}
+
+pool();
