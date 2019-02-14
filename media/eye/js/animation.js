f8c0c6d8fecb399b21dab93f9d8c0f6ee2a2f8b4
Людмила Теплова <l.teplova@teplova.drweb.lan>
2019-02-14 10:53:29 +0300

    animation

--- media/eye/js/animation.js
+++ media/eye/js/animation.js
@@ -1,1 +1,35 @@
 'use strict';
+
+
+const eye = document.querySelector('.big-book__eye');
+const pupil = document.querySelector('.big-book__pupil');
+const siteSize = document.body.getBoundingClientRect();
+
+const pupilW = pupil.clientWidth;
+const pupilH = pupil.clientHeight;
+const pupilR = pupil.clientWidth / 2;
+
+//Метод Element.getBoundingClientRect() возвращает размер элемента и его позицию относительно окна.
+
+
+window.addEventListener('mousemove', event => {
+    //eye.style.setProperty('--pupil-size', pupilSize());
+
+ // Позиция курсора мыши
+ const mousePos = {
+    x: event.pageX,
+    y: event.pageY
+  };
+  console.log(mousePos.x, mousePos.y);
+});
+
+ // Размеры и позиция глазного яблока
+const eyeBallBCR = eyeBall.getBoundingClientRect();
+const eyeBallSize = {
+  width: eyeBallBCR.width,
+  height: eyeBallBCR.height
+};
+
+
+
+
