cf2c1ed4c7885e214fb75234cf51c26def9d3e15
Liudmila Teplova <lteplova@gmail.com>
2019-02-17 12:35:56 +0300

    File Editor done

--- file-api/text-editor/js/main.js
+++ file-api/text-editor/js/main.js
@@ -1,44 +1,59 @@
-const throttle = ( handler, ms ) => {
+const throttle = (handler, ms) => {
   let timeout;
   return () => {
-    clearTimeout( timeout );
-    timeout = setTimeout( handler, ms );
-  }
+    clearTimeout(timeout);
+    timeout = setTimeout(handler, ms);
+  };
 };
 class TextEditor {
-  constructor( container, storageKey = '_text-editor__content' ) {
+  constructor(container, storageKey = "_text-editor__content") {
     this.container = container;
-    this.contentContainer = container.querySelector( '.text-editor__content' );
-    this.hintContainer = container.querySelector( '.text-editor__hint' );
-    this.filenameContainer = container.querySelector( '.text-editor__filename' );
+    this.contentContainer = container.querySelector(".text-editor__content");
+    this.hintContainer = container.querySelector(".text-editor__hint");
+    this.filenameContainer = container.querySelector(".text-editor__filename");
     this.storageKey = storageKey;
     this.registerEvents();
-    this.load( this.getStorageData());
+    this.load(this.getStorageData());
   }
   registerEvents() {
-    const save = throttle( this.save.bind( this ), 1000 );
-    this.contentContainer.addEventListener( 'input', save );
+    const save = throttle(this.save.bind(this), 1000);
+    this.contentContainer.addEventListener("input", save);
+    this.container.addEventListener("drop", event => this.loadFile(event));
+    this.container.addEventListener("dragover", event => this.showHint(event));
   }
-  loadFile( e ) {
+  loadFile(e) {
+    e.preventDefault();
+    const file = Array.from(e.dataTransfer.files)[0];
+    this.readFile(file);
   }
-  readFile( file ) {
+  readFile(file) {
+    const reader = new FileReader();
+    reader.addEventListener("load", event => {
+      this.hideHint();
+      this.filenameContainer.innerText = file.name;
+      this.contentContainer.value = event.target.result;
+    });
+    reader.readAsText(file);
   }
-  setFilename( filename ) {
+  setFilename(filename) {
     this.filenameContainer.textContent = filename;
   }
-  showHint( e ) {
+  showHint(e) {
+    e.preventDefault();
+    this.hintContainer.classList.add("text-editor__hint_visible");
   }
   hideHint() {
+    this.hintContainer.classList.remove("text-editor__hint_visible");
   }
-  load( value ) {
-    this.contentContainer.value = value || '';
+  load(value) {
+    this.contentContainer.value = value || "";
   }
   getStorageData() {
-    return localStorage[ this.storageKey ];
+    return localStorage[this.storageKey];
   }
   save() {
-    localStorage[ this.storageKey ] = this.contentContainer.value;
+    localStorage[this.storageKey] = this.contentContainer.value;
   }
 }
 
-new TextEditor( document.getElementById( 'editor' ));
+new TextEditor(document.getElementById("editor"));
